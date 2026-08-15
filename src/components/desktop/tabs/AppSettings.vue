f957e40dc5ee19dac283ccac42434890f76f695f
smh0505 <mson2017@my.fit.edu>
2026-08-15 14:58:56 -0400

    Revert "polish: right-align language dropdown trigger text"
    This reverts commit 99cb9cc4e132b13cd9fad82803b1725f2147b8af.

--- src/components/desktop/tabs/AppSettings.vue
+++ src/components/desktop/tabs/AppSettings.vue
@@ -331,7 +331,6 @@ onBeforeUnmount(stopListeningForHotkey);
 .language-menu-trigger {
   width: 100%;
   justify-content: space-between;
-  text-align: right;
 }
 
 /* No right:0 override (unlike PluginSettings.vue's tabs-dropdown-panel) - that stretched the
